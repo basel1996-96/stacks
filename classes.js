7cc621fd61f75803e389e9e3935a6ad6e4d638c3
Basel Abu Tarboush <babutarboush@premierlending.com>
2021-05-27 03:41:37 -0700

    updated

--- classes.js
+++ classes.js
@@ -1,90 +1,70 @@
-
-
+const colors = ["red", "blue", "green", "yellow"];
 
 class Node {
-  constructor(color, number, next = null) {
+  constructor(color, num, nextCard = null) {
     this.color = color;
-    this.number = number;
-    this.next = next;
+    this.num = num;
+    this.nextCard = nextCard;
   }
   getData() {
-     return `${this.color} - ${this.num}`;
+    return `${this.color} , ${this.num}`;
   }
 }
 
-class Stack{
-    constructor (limit =20){
-        this.top = null
-        this.limit = limit 
-        this.size = 0
+class cardDeck {
+  constructor() {
+    this.topCard = null;
+  }
 
+  push = (color, num) => {
+    const newCard = new Node(color, num);
+    newCard.nextCard = this.topCard;
+    this.topCard = newCard;
+  };
+
+  pop = () => {
+    const popped = this.topCard;
+    this.topCard = popped.nextCard;
+    return `${popped.color} -  ${popped.num}`;
+  };
+
+  print = () => {
+    let cur = this.topCard;
+    while (cur) {
+      console.log(`card  : ${cur.color} , ${cur.num}`);
+      cur = cur.nextCard;
     }
-    isEmpty = () => this.size === 0
+  };
 
-    isFull = () => this.size === this.limit
-    
-    peek = () => this.top.getData()
-
-    pop = () => {
-        if (this.isEmpty())
-        console.log("the stack is empty ")
-        else {
-            const removedNode = this.top
-            this.top = removedNode.next
-            this.size--
-            return removedNode.data
-        }
-    
-    }
-     push = (color,number)=> {
-         if (this.isFull())
-         console.log ("full stack")
-         else {
-             const newNode = new Node(color,number)
-             newNode.next = this.top
-             this.top = newNode
-             this.size++
-         }
-     }
-     displayData= () => {
-         let currentNode = this.top
-         while(currentNode){
-             console.log(`number : ${currentNode.number} , color : ${current.Node.color}`)
-         }
-     }
-}
-const colors = ["red", "blue", "green", "yellow"];
-const numbers = [ 1,2,3,4,5,6,7,8,9]
-const random = (array)=> {
-    const index = Math.floor(Math.random() * array.length)
-    return array [index]
-
-}
-const cardsStack = new Stack(20)
-const cardsArray = []
-const cards = new Stack (20)
-
-while (!cards.isFull () ){
-    const number = random(numbers)
-    const color = random(colors)
-    if(!cardsArray.includes(`${color},${number}`)) {
-        cardsStack.push(color,number)
-        cardsArray.push (`${color},${number}`)
-    }
-}
-console.log ("Deck :")
-cardsStack.displayData()
-let palyer1 = []
-let palyer2 =[]
-
-let i = 0
-
-while (i<5){
-
-    player1.push(cardsStack.pop())
-    player2.push(cardsStack.pop())
-    i++
+  get peek() {
+    return ` card color : ${his.topCard.color}  card number : ${this.topCard.num}`;
+  }
 }
 
-console.log ("player cards:" ,player1)
-console.log ("player cards:",player2)
+const deck = new cardDeck();
+
+let c = 0;
+while (c < 20) {
+  let randomColor = colors[Math.floor(Math.random() * colors.length)];
+
+  let randomNum = Math.floor(Math.random() * 10);
+
+  deck.push(randomColor, randomNum);
+  c++;
+}
+
+deck.print();
+
+const player1 = [];
+const player2 = [];
+
+let x = 0;
+while (x < 5) {
+  player1.push(deck.pop());
+  player2.push(deck.pop());
+
+  x++;
+}
+
+console.log(player1);
+console.log(player2);
